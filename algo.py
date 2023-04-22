785883e4238384256bbdf688505f33495e5e2ded
eswat109 <eswat98@mail.ru>
2023-04-22 22:16:36 +0300

    фикс алгоритмов

--- algo.py
+++ algo.py
@@ -6,13 +6,13 @@ from matplotlib import pyplot as plt
 class Solver:
 
     '''ARTICLE PARAMETERS'''
-    SIFT_peak_threshold = 0.05 #0.08
+    SIFT_peak_threshold = 0.07 #0.08
     SIFT_edge_threshold = 10
 
-    SIFT_feature_elimination_threshold = 5
+    SIFT_feature_elimination_threshold = 10
     SIFT_matching_threshold = 2
 
-    samson_err = 0.9 #0.5
+    samson_err = 0.3 #0.5
 
     RANSAC_nIter = 2000
 
@@ -25,62 +25,74 @@ class Solver:
     blur_on = True
     g_blur_on = True
     sized_on = False
-    intens_on = False
+    intens_on = True
     is_thrs_on = True
     point_size = 30
     ED_size = point_size #30
     AD_size = point_size  #
 
-    def makeFieldByPoints(this, img, pts, intns = None):
+    def __init__(self, SIFT_peak=0.8, SIFT_edge=10, Samson_err=0.5, Ratio=0.7, G_deviation=30, P_size=30):
+        self.SIFT_peak_threshold = SIFT_peak
+        self.SIFT_edge_threshold = SIFT_edge
+        self.samson_err = Samson_err
+        self.ratio_test = Ratio
+        self.Gaussian_deviation = G_deviation
+        self.point_size = P_size
+
+    def makeFieldByPoints(self, img, pts, intns = None):
         blank_image = np.zeros(img.shape, np.uint8)
         maxIntns = 255
         for i, pt in enumerate(pts):
             color = 1
-            if this.intens_on:
+            if self.intens_on:
                 if intns is not None:
                     color = intns[i]
-            blank_image = cv.circle(blank_image, tuple(pt), this.point_size, color * maxIntns, -1)
-        if not this.blur_on:
+            blank_image = cv.circle(blank_image, tuple(pt), self.point_size, color * maxIntns, -1)
+        if not self.blur_on:
             return blank_image
-        if this.g_blur_on:
-            return cv.GaussianBlur(blank_image, (this.Gaussian_kernel, this.Gaussian_kernel), this.Gaussian_deviation)
-        return cv.blur(blank_image, (this.Gaussian_kernel, this.Gaussian_kernel), this.Gaussian_deviation)
+        if self.g_blur_on:
+            return cv.GaussianBlur(blank_image, (self.Gaussian_kernel, self.Gaussian_kernel), self.Gaussian_deviation)
+        return cv.blur(blank_image, (self.Gaussian_kernel, self.Gaussian_kernel), self.Gaussian_deviation)
 
-    def makeImgFromField(this, field):
+    def makeImgFromField(self, field):
         maxIntns = 255
         return np.multiply(maxIntns, field)
 
-    def makeFieldFromIg(this, field):
+    def makeFieldFromIg(self, field):
         maxIntns = 255
         return np.divide(field, maxIntns)
 
-    def andMasks(this, m1, m2):
-        #cv.multiply(m1, m2)
+    def andMasks(self, m1, m2):
+        m = cv.multiply(m1, m2)
 
+        '''
         r1 = m1.astype(float) / 255
         r2 = m2.astype(float) / 255
         r = np.multiply(r1, r2)
         m = r * 255
         m = m.astype(np.uint8)
-        if this.is_thrs_on:
-            _, m = cv.threshold(m, int(this.Specular_thr * 255), 255, cv.THRESH_BINARY)
+        '''
+
+        if self.is_thrs_on:
+            _, m = cv.threshold(m, int(self.Specular_thr * 255), 255, cv.THRESH_BINARY)
         return m
 
-    def getMasksFromFrames(this, img1, img2):
+    def getMasksFromFrames(self, img1, img2):
         ''' MAKE SPECULAR MASK FROM TWO CONSISTENT FRAMES '''
 
         # SIFT CREATE
-        sift = cv.SIFT_create(contrastThreshold=this.SIFT_peak_threshold, edgeThreshold=this.SIFT_edge_threshold)
+        sift = cv.SIFT_create(contrastThreshold=self.SIFT_peak_threshold, edgeThreshold=self.SIFT_edge_threshold)
         kp1, des1 = sift.detectAndCompute(img1, None)
         kp2, des2 = sift.detectAndCompute(img2, None)
 
         # AVERAGE VECTOR THRESHOLD
-        '''av1 = [np.average(des) for des in des1]
+        av1 = [np.average(des) for des in des1]
         flag = 0
         for d in av1:
-            if d <= 10:
+            if d <= self.SIFT_feature_elimination_threshold:
                 flag += 1
-        print(flag)'''
+        if flag > 0:
+            print(flag)
 
         # FLANN parameters
         FLANN_INDEX_KDTREE = 1
@@ -89,7 +101,7 @@ class Solver:
 
         # KDTREE MATCHING
         flann = cv.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(des1, des2, k=this.SIFT_matching_threshold)
+        matches = flann.knnMatch(des1, des2, k=self.SIFT_matching_threshold)
         pts1 = []
         pts2 = []
         kpts1 = []
@@ -100,7 +112,7 @@ class Solver:
 
         # ratio test as per Lowe's paper
         for i, (m, n) in enumerate(matches):
-            if m.distance < this.ratio_test * n.distance:
+            if m.distance < self.ratio_test * n.distance:
                 good.append(m)
                 pts2.append(kp2[m.trainIdx].pt)
                 pts1.append(kp1[m.queryIdx].pt)
@@ -118,8 +130,8 @@ class Solver:
 
 
         # FUNDAMENTAL MATRIX CALCULATION
-        F, mask = cv.findFundamentalMat(pts1, pts2, cv.RANSAC, ransacReprojThreshold=this.samson_err, confidence=0.995,
-                                        maxIters=this.RANSAC_nIter)
+        F, mask = cv.findFundamentalMat(pts1, pts2, cv.RANSAC, ransacReprojThreshold=self.samson_err, confidence=0.995,
+                                        maxIters=self.RANSAC_nIter)
         # INLINERS
         inl_pts1 = pts1[mask.ravel() == 1]
         inl_pts2 = pts2[mask.ravel() == 1]
@@ -150,17 +162,17 @@ class Solver:
                 outl_kpts1.append(kpts1[i])
                 outl_kpts2.append(kpts2[i])
 
-        ED_mask = this.makeFieldByPoints(img2, outl_pts2)
-        AD_mask = this.makeFieldByPoints(img2, inl_pts2, des_l1_norm)
+        ED_mask = self.makeFieldByPoints(img2, outl_pts2)
+        AD_mask = self.makeFieldByPoints(img2, inl_pts2, des_l1_norm)
 
-        SPEC_mask = this.andMasks(ED_mask, AD_mask)
+        SPEC_mask = self.andMasks(ED_mask, AD_mask)
 
         '''if is_thrs_on:
             _, SPEC_mask = cv.threshold(SPEC_mask, Specular_thr, 255, cv.THRESH_TOZERO)'''
 
         return (SPEC_mask, ED_mask, AD_mask)
 
-    def combineFrameAndMask(this, img, mask):
+    def combineFrameAndMask(self, img, mask):
         #color = np.array([255, 0, 255])
         #white_mask = cv.cvtColor(makeImgFromField(mask), cv.COLOR_GRAY2RGB)
         white_mask = cv.cvtColor(mask, cv.COLOR_GRAY2RGB)
@@ -170,30 +182,30 @@ class Solver:
         res = cv.addWeighted(img, 1, color_mask, 0.7, 0.0)
         return res
 
-    def getMaskedImage(this, img1, img2):
-        SPEC_mask, ED_mask, AD_mask = this.getMasksFromFrames(img1, img2)
-        img_mask = this.combineFrameAndMask(img2_2, SPEC_mask)
+    def getMaskedImage(self, img1, img2):
+        SPEC_mask, ED_mask, AD_mask = self.getMasksFromFrames(img1, img2)
+        img_mask = self.combineFrameAndMask(img2_2, SPEC_mask)
         return img_mask
 
-    def findContour(this, img, mask):
+    '''def findContour(self, img, mask):
         contours, hierarchy = cv.findContours(image=mask, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
         image_copy = img.copy()
         cv.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv.LINE_AA)
         print(cv.contourArea(contours[0]))
-        return image_copy
+        return image_copy'''
 
-    def getMaskArea(this, mask):
+    def getMaskArea(self, mask):
         contours, hierarchy = cv.findContours(image=mask, mode=cv.RETR_LIST, method=cv.CHAIN_APPROX_NONE)
         totalArea = 0.0
         for c in contours:
             totalArea += cv.contourArea(c)
         return totalArea
 
-    def getPrecisionRecall(this, maskSpec, maskTrue):
+    def getPrecisionRecall(self, maskSpec, maskTrue):
         maskIntersec = cv.bitwise_and(maskSpec, maskTrue)
-        I = this.getMaskArea(maskIntersec)
-        D = this.getMaskArea(maskSpec)
-        G = this.getMaskArea(maskTrue)
+        I = self.getMaskArea(maskIntersec)
+        D = self.getMaskArea(maskSpec)
+        G = self.getMaskArea(maskTrue)
         Precision = I / D
         Recall = I / G
         return (Precision, Recall)
